3f000860bf702be9745a8ff4411c27dae267b4fa
ssili126 <112532343+ssili126@users.noreply.github.com>
2024-01-15 16:32:15 +0800

    Update IPTV-hebei.py

--- IPTV-hebei.py
+++ IPTV-hebei.py
@@ -70,7 +70,7 @@ for url in urls:
                         name = name.replace("CCTV9记录", "CCTV9")
                         name = name.replace("CCTV9纪录", "CCTV9")
                         name = name.replace("CCTV10科教", "CCTV10")
-                        name = name.replace("CCTV11戏曲"， "CCTV11")
+                        name = name.replace("CCTV11戏曲", "CCTV11")
                         name = name.replace("CCTV12社会与法", "CCTV12")
                         name = name.replace("CCTV13新闻", "CCTV13")
                         name = name.replace("CCTV新闻", "CCTV13")
