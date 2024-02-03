656dc3a2abc2b55cc8130e5c17e1020c64bd0e07
ssili126 <ssili@126.com>
2024-02-03 09:45:54 +0800

    更新

--- IPTV-hebei.py
+++ IPTV-hebei.py
@@ -3,16 +3,51 @@ import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
-import json
 import re
+import os
+import threading
+from queue import Queue
+
+urls = [
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSGViZWki",  # Hebei (河北)
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iYmVpamluZyI%3D",  # Beijing (北京)
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rKz5Y2XIg%3D%3D",  # 河南
+    "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iZ3Vhbmdkb25nIg%3D%3D",  # Guangdong (广东)
+    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22hebei%22",        #河北
+    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22beijing%22",   #北京
+    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22henan%22",    #河南
+    "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22guangdong%22"    #广东
+    ]
+
+def modify_urls(url):
+    modified_urls = []
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "/iptv/live/1000.json?key=txiptv"
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
+
+    return modified_urls
 
 
-hebei = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSGViZWki"        #河北
-cn = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0ic2hhbmRvbmci"   #山东
-#hebei = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iYmFvZGluZyI%3D"        #保定
-#cn = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaGFuZGFuIg%3D%3D"   #邯郸
+def is_url_accessible(url):
+    try:
+        response = requests.get(url, timeout=0.5)
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
 
-def process_url(url):
+
+results = []
+
+for url in urls:
     # 创建一个Chrome WebDriver实例
     chrome_options = Options()
     chrome_options.add_argument('--headless')
@@ -32,10 +67,10 @@ def process_url(url):
     # 查找所有符合指定格式的网址
     pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
     urls_all = re.findall(pattern, page_content)
-    #urls = list(set(urls_all))  # 去重得到唯一的URL列表
+    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
     urls = set(urls_all)  # 去重得到唯一的URL列表
     x_urls = []
-    for url in urls: # 对urls进行处理，ip第四位修改为1，并去重
+    for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
         url = url.strip()
         ip_start_index = url.find("//") + 2
         ip_end_index = url.find(":", ip_start_index)
@@ -49,11 +84,11 @@ def process_url(url):
         modified_ip = f"{ip_address}{ip_end}"
         x_url = f"{base_url}{modified_ip}{port}"
         x_urls.append(x_url)
-    urls = set(x_urls) # 去重得到唯一的URL列表
+    urls = set(x_urls)  # 去重得到唯一的URL列表
 
     valid_urls = []
     #   多线程获取可用url
-    with concurrent.futures.ThreadPoolExecutor(max_workers = 100) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
         futures = []
         for url in urls:
             url = url.strip()
@@ -69,7 +104,6 @@ def process_url(url):
     for url in valid_urls:
         print(url)
     # 遍历网址列表，获取JSON文件并解析
-    results = []
     for url in valid_urls:
         try:
             # 发送GET请求获取JSON文件，设置超时时间为0.5秒
@@ -79,7 +113,7 @@ def process_url(url):
             base_url = url[:ip_start_index]  # http:// or https://
             ip_address = url[ip_start_index:ip_index_second]
             url_x = f"{base_url}{ip_address}"
-            
+
             json_url = f"{url}"
             response = requests.get(json_url, timeout=0.5)
             json_data = response.json()
@@ -90,13 +124,15 @@ def process_url(url):
                     if isinstance(item, dict):
                         name = item.get('name')
                         urlx = item.get('url')
-                        if 'http' in urlx.lower():
+                        #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
+                        if 'http' in urlx:
                             urld = f"{urlx}"
                         else:
                             urld = f"{url_x}{urlx}"
 
                         if name and urlx:
                             # 删除特定文字
+                            name = name.replace("cctv", "CCTV")
                             name = name.replace("中央", "CCTV")
                             name = name.replace("高清", "")
                             name = name.replace("HD", "")
@@ -108,6 +144,7 @@ def process_url(url):
                             name = name.replace("＋", "+")
                             name = name.replace("(", "")
                             name = name.replace(")", "")
+                            name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
                             name = name.replace("CCTV1综合", "CCTV1")
                             name = name.replace("CCTV2财经", "CCTV2")
                             name = name.replace("CCTV3综艺", "CCTV3")
@@ -130,66 +167,173 @@ def process_url(url):
                             name = name.replace("CCTV15音乐", "CCTV15")
                             name = name.replace("CCTV16奥林匹克", "CCTV16")
                             name = name.replace("CCTV17农业农村", "CCTV17")
+                            name = name.replace("CCTV17农业", "CCTV17")
                             name = name.replace("CCTV5+体育赛视", "CCTV5+")
                             name = name.replace("CCTV5+体育赛事", "CCTV5+")
-                            if 'udp' in urld or 'rtp' in urld:
-                                pass
-                            else:
-                                results.append(f"{name},{urld}")
+                            name = name.replace("CCTV5+体育", "CCTV5+")
+                            results.append(f"{name},{urld}")
             except:
                 continue
         except:
             continue
 
-    return results
-def save_results(results, filename):
-    # 将结果保存到文本文件
-    with open(filename, "w", encoding="utf-8") as file:
-        for result in results:
-            file.write(result + "\n")
-            print(result)
 
-def modify_urls(url):
-    modified_urls = []
-    ip_start_index = url.find("//") + 2
-    ip_end_index = url.find(":", ip_start_index)
-    base_url = url[:ip_start_index]  # http:// or https://
-    ip_address = url[ip_start_index:ip_end_index]
-    port = url[ip_end_index:]
-    ip_end = "/iptv/live/1000.json?key=txiptv"
-    for i in range(1, 256):
-        modified_ip = f"{ip_address[:-1]}{i}"
-        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
-        modified_urls.append(modified_url)
+channels = []
 
-    return modified_urls
+for result in results:
+    line = result.strip()
+    if result:
+        channel_name, channel_url = result.split(',')
+        channels.append((channel_name, channel_url))
 
-def is_url_accessible(url):
-    try:
-        response = requests.get(url, timeout=0.5)
-        if response.status_code == 200:
-            return url
-    except requests.exceptions.RequestException:
-        pass
+import eventlet
+eventlet.monkey_patch()
 
-    return None
+# 线程安全的队列，用于存储下载任务
+task_queue = Queue()
 
-# 处理第1个URL
-results_hebei = process_url(hebei)
-save_results(results_hebei, "hebei.txt")
+# 线程安全的列表，用于存储结果
+results = []
 
-# 处理第2个URL
-results_cn = process_url(cn)
-save_results(results_cn, "cn.txt")
+error_channels = []
+
+
+# 定义工作线程函数
+def worker():
+    while True:
+        # 从队列中获取一个任务
+        channel_name, channel_url = task_queue.get()
+        try:
+            channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
+            lines = requests.get(channel_url).text.strip().split('\n')  # 获取m3u8文件内容
+            ts_lists = [line.split('/')[-1] for line in lines if line.startswith('#') == False]  # 获取m3u8文件下视频流后缀
+            ts_lists_0 = ts_lists[0].rstrip(ts_lists[0].split('.ts')[-1])  # m3u8链接前缀
+            ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
+
+            # 多获取的视频数据进行5秒钟限制
+            with eventlet.Timeout(5, False):
+                start_time = time.time()
+                content = requests.get(ts_url).content
+                end_time = time.time()
+                response_time = (end_time - start_time) * 1
+
+            if content:
+                with open(ts_lists_0, 'ab') as f:
+                    f.write(content)  # 写入文件
+                file_size = len(content)
+                # print(f"文件大小：{file_size} 字节")
+                download_speed = file_size / response_time / 1024
+                # print(f"下载速度：{download_speed:.3f} kB/s")
+                normalized_speed = min(max(download_speed / 1024, 0.001), 100)  # 将速率从kB/s转换为MB/s并限制在1~100之间
+                #print(f"标准化后的速率：{normalized_speed:.3f} MB/s")
+
+                # 删除下载的文件
+                os.remove(ts_lists_0)
+                result = channel_name, channel_url, f"{normalized_speed:.3f} MB/s"
+                results.append(result)
+                numberx = (len(results) + len(error_channels)) / len(channels) * 100
+                print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+        except:
+            error_channel = channel_name, channel_url
+            error_channels.append(error_channel)
+            numberx = (len(results) + len(error_channels)) / len(channels) * 100
+            print(f"可用频道：{len(results)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(channels)} 个 ,总进度：{numberx:.2f} %。")
+
+        # 标记任务完成
+        task_queue.task_done()
+
+
+# 创建多个工作线程
+num_threads = 10
+for _ in range(num_threads):
+    t = threading.Thread(target=worker, daemon=True)  # 将工作线程设置为守护线程
+    t.start()
+
+# 添加下载任务到队列
+for channel in channels:
+    task_queue.put(channel)
+
+# 等待所有任务完成
+task_queue.join()
+
+
+def channel_key(channel_name):
+    match = re.search(r'\d+', channel_name)
+    if match:
+        return int(match.group())
+    else:
+        return float('inf')  # 返回一个无穷大的数字作为关键字
+
+# 对频道进行排序
+results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
+results.sort(key=lambda x: channel_key(x[0]))
+
+# 将结果写入文件
+with open("itv_all_results.txt", 'w', encoding='utf-8') as file:
+    for result in results:
+        channel_name, channel_url, speed = result
+        file.write(f"{channel_name},{channel_url},{speed}\n")
+
+with open("itv_all_speed.txt", 'w', encoding='utf-8') as file:
+    for result in results:
+        channel_name, channel_url, speed = result
+        file.write(f"{channel_name},{channel_url}\n")
+
+
+result_counter = 5  # 每个频道需要的个数
+
+with open("itv_all.txt", 'w', encoding='utf-8') as file:
+    channel_counters = {}
+    file.write('央视频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if 'CCTV' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+    channel_counters = {}
+    file.write('卫视频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if '卫视' in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+    channel_counters = {}
+    file.write('其他频道,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
 
 # 合并文件内容
 file_contents = []
-file_paths = ["cy.txt", "hebei.txt", "cn.txt"]  # 替换为实际的文件路径列表
+file_paths = ["cy.txt", "itv_all.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
         file_contents.append(content)
 
 # 写入合并后的文件
-with open("tv.txt", "w", encoding="utf-8") as output:
+with open("itv.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
