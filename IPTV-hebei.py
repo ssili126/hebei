3671211a3a5bc433abcb14974eda383ccf4edd3d
ssili126 <112532343+ssili126@users.noreply.github.com>
2024-02-08 20:35:43 +0800

    更新 IPTV-hebei.py

--- IPTV-hebei.py
+++ IPTV-hebei.py
@@ -185,7 +185,7 @@ channels = []
 for result in results:
     line = result.strip()
     if line:
-        channel_name, channel_url = line
+        channel_name,channel_url = line
         channels.append((channel_name, channel_url))
 
 # 线程安全的队列，用于存储下载任务
